e2aa1c7ce467a18c5c2be6084244b24f4022935f
hyesun03 <chsun0303@gmail.com>
2019-10-03 20:56:13 +0900

    json 읽는 부분이랑 가공하는 부분 함수로 쪼개놓음

--- pickme/hyesun03/main.py
+++ pickme/hyesun03/main.py
@@ -1,24 +1,32 @@
+from typing import List
 import json
 import random
 
 
-with open("study_member.json") as file:
-    data = json.load(file)
-    study_member = data["members"]
+def read_json() -> List[list]:
+    with open("study_member.json") as file:
+        data = json.load(file)
+        study_member = data["members"]
 
-    name, speak, wiki, absent = [], [], [], []
-    for member in study_member:
-        name.append(member["name"])
-        speak.append(len(member["speak"]))
-        wiki.append(len(member["wiki"]))
-        absent.append(len(member["absent"]))
+        name, speak, wiki, absent = [], [], [], []
+        for member in study_member:
+            name.append(member["name"])
+            speak.append(len(member["speak"]))
+            wiki.append(len(member["wiki"]))
+            absent.append(len(member["absent"]))
+
+        return [name, speak, wiki, absent]
+
+
+def pickme() -> List[str]:
+    name, speak, wiki, absent = read_json()
 
     # 현재까지 진행한 스터디 횟수 == 발표 횟수
     total = len(speak)
 
     # 1 - ((내가 걸린 횟수 - 내가 결석한 횟수) / 여태 진행했던 횟수) 로 가중치 적용
-    speak = [1 - (speak[i] - absent[i]) / total for i in range(len(study_member))]
-    wiki = [1 - (wiki[i] - absent[i]) / total for i in range(len(study_member))]
+    speak = [1 - (speak[i] - absent[i]) / total for i in range(len(name))]
+    wiki = [1 - (wiki[i] - absent[i]) / total for i in range(len(name))]
 
     # 오늘 결석한 사람
     today_absent = ["윤지수"]
@@ -34,6 +42,12 @@ with open("study_member.json") as file:
 
     while today_wikier in today_absent or today_wikier == today_speaker:
         today_wikier = random.choices(name, wiki, k=1)[0]
+
+    return [today_speaker, today_wikier]
+
+
+if __name__ == '__main__':
+    today_speaker, today_wikier = pickme()
 
     print("<< 축 하 합 니 다 >>")
     print("발표자:\t", today_speaker)
