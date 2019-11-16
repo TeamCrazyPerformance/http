b0d2cae67cf915c0acb31a34ef622f263e340f45
DESKTOP-4OOTLQN\kjhmd <kjhmd9726@gmail.com>
2019-11-17 03:09:24 +0900

    update pickPresenterComponent

--- pickme/KangJiJi/src/components/pickPresenterComponent.js
+++ pickme/KangJiJi/src/components/pickPresenterComponent.js
@@ -3,18 +3,26 @@ import domHelper from './../helper/domHelper';
 var pickPresenterComponent = {
   componentHtml: `<input type="button" class="pick-presenter-component__pick-presenter-button" value="pick">`,
   componentClassName: `pick-presenter-component`,
+  eventHandler: Function.prototype,
   getComponentHtml: function() {
     return this.componentHtml;
   },
   getComponentClassName: function() {
     return this.componentClassName;
   },
-  addEvent: function() {
-    console.log('pickPresenterComponent');
+  setEventHandler: function(eventHandler) {
+    this.eventHandler = eventHandler;
   },
-  render: function(renderingOption) {
-    domHelper.render(this, renderingOption);
-    this.addEvent();
+  pickPresenter: function() {
+    return this.eventHandler();
+  },
+  addEvent: function() {
+    let button = document.getElementsByClassName('pick-presenter-component__pick-presenter-button')[0];
+    button.addEventListener('click', this.pickPresenter.bind(this));
+  },
+  render: function(eventHandler) {
+    this.setEventHandler(eventHandler);
+    domHelper.render(this);
   }
 }
 
