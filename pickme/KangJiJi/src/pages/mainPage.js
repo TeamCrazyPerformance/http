46a860bb59189f591e1d90b770e23c2afa44c633
DESKTOP-4OOTLQN\kjhmd <kjhmd9726@gmail.com>
2019-11-17 03:09:53 +0900

    update mainPage component

--- pickme/KangJiJi/src/pages/mainPage.js
+++ pickme/KangJiJi/src/pages/mainPage.js
@@ -4,11 +4,34 @@ import pickPresenterComponent from './../components/pickPresenterComponent';
 import showPresenterComponent from './../components/showPresenterComponent';
 
 var mainPage = {
+  numOfParticipant: 1,
+  InformationOfParticipant: Array.prototype,
+  setNumOfParticipant: function(numOfParticipant) {
+    this.numOfParticipant = numOfParticipant;
+  },
+  setInformationOfParticipant: function(InformationOfParticipant) {
+    this.InformationOfParticipant = InformationOfParticipant;
+  },
+  appendSetInformationOfParticipantComponent: function() {
+    setInformationOfParticipantComponent.render();
+  },
+  changeEventNumOfParticipant: function(numOfParticipant) {
+    this.setNumOfParticipant(numOfParticipant);
+    this.appendSetInformationOfParticipantComponent();
+  },
+  pickPresenter: function() {
+    let informationOfParticipantWrapperDom = document.getElementsByClassName(setInformationOfParticipantComponent.getComponentClassName())[0];
+    let informationOfParticipantDom = Array.from(informationOfParticipantWrapperDom.children);
+    console.log(informationOfParticipantDom);
+
+    // this.setInformationOfParticipant(setInformationOfParticipantComponent.getInformationOfParticipant());
+    // pick presenter logic
+  },
   render: function() {
-    setNumOfParticipantComponent.render('append');
-    setInformationOfParticipantComponent.render('append');
-    pickPresenterComponent.render('append');
-    showPresenterComponent.render('append');
+    setNumOfParticipantComponent.render(this.changeEventNumOfParticipant.bind(this));
+    setInformationOfParticipantComponent.render();
+    pickPresenterComponent.render(this.pickPresenter.bind(this));
+    showPresenterComponent.render();
   }
 }
 
