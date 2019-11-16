ef3c47784346f81d448ac39ab591a42f1216088b
DESKTOP-4OOTLQN\kjhmd <kjhmd9726@gmail.com>
2019-11-17 03:07:33 +0900

    update domHelper

--- pickme/KangJiJi/src/helper/domHelper.js
+++ pickme/KangJiJi/src/helper/domHelper.js
@@ -1,24 +1,43 @@
 var domHelper = {
-  render: function(component, renderingOption) {
-    this.innerHtml(component, renderingOption)
+  render: function(component) {
+    this.appendHtml(component)
+    .then(this.addEvent)
     .catch(function(err) {
       console.log("Error:", err);
     });
   },
-  innerHtml: function(component, renderingOption) {
+  appendHtml: function(component) {
     return new Promise(function (resolve, reject) {
       if(!component.getComponentHtml) { reject(`This component does not have getComponentHtml method`); }
 
-      var main = document.getElementById("app");
+      let parentComponent = document.getElementById("app");
+      let divWrapper = document.createElement("div");
 
-      var divWrapper = document.createElement("div");
-      divWrapper.className = component.getComponentClassName();
-      divWrapper.innerHTML = component.getComponentHtml();
-
-      main.appendChild(divWrapper);
+      if(!!document.getElementsByClassName(component.getComponentClassName())[0]) {
+        // Append
+        parentComponent = document.getElementsByClassName(component.getComponentClassName())[0];
+        divWrapper.innerHTML = component.getComponentHtml();
+        parentComponent.appendChild(divWrapper.children[0]);
+      } else {
+        // InnerHTML is remove previous event listener.
+        divWrapper.className = component.getComponentClassName();
+        divWrapper.innerHTML = component.getComponentHtml();
+        parentComponent.appendChild(divWrapper);
+      }
 
       resolve(component);
     });
+  },
+  addEvent: function(component) {
+    return new Promise(function(resolve, reject) {
+      if(!component.addEvent) { reject(`This component does not have addEvent method`); }
+      
+      component.addEvent();
+      resolve(component);
+    });
+  },
+  removeHtml: function() {
+    console.log('This function remove component by class name');
   },
   initDom: function() {
     document.getElementById("app").innerHTML = ``;
