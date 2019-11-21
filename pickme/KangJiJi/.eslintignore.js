28e65bc71418b3ad767fb96a05dd83993d7210c8
KangJi <kjhmd9726@gmail.com>
2019-11-21 23:40:40 +0900

    Revert "PR"

--- pickme/KangJiJi/.eslintignore.js
+++
@@ -1,7 +0,0 @@
-/node_modules/**
-/.cache/**
-/dist/**
-/.babelrc
-/.postcssrc
-/package-lock.json
-/package.json
--- pickme/KangJiJi/index.js
+++
@@ -1,3 +0,0 @@
-import app from './src/app';
-
-window.addEventListener('load', app.render.bind(app));
